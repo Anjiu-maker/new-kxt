b56b81ddd112f6163b0f6dc0ae9dfe80fd8db193
lcx <lu17354778157@163.com>
2026-05-02 23:17:00 +0800

    element 语言改为中文

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './assets/fonts/icons/iconfont.css'
@@ -16,7 +17,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: zhCn })
 app.use(pinia)
 app.use(router)
 app.mount('#app')
